6dea206d5c3642781401f3015c5444216001f05b
ycuige1 <yuqing.cui@u-psud.fr>
2018-11-14 23:52:19 +0100

    fini exo43

--- src/main.c
+++ src/main.c
@@ -2,8 +2,42 @@
 //
 
 #include "my_header.h"
-
+#include "stdlib.h"
 int main(void)
 {
+ unsigned long int n=0,S1=0,r,S2=0;S3=0,S4=0; 
+ unsigned short int m=0,i=0,a=0,b=0,p=0;
+
+   while(n<9999999)
+   { 
+     S1=S1+n; 
+     n=n+3; 
+   } 
+   while(n2<9876)
+   { 
+     S2=S2+m; 
+      m=m+3; 
+   } 
+   while(i<13)
+   { 
+     r=(rand()%10)+1;
+     S3=S3+r; 
+     i=i+1; 
+   } 
+   while(a<21)
+   { 
+     if(a%3==0||b%7==0) 
+      {
+          S4=S4+a; 
+          p=p+1; 
+          a=a+1; 
+          b=b+1; 
+      } 
+     else 
+      { 
+      a=a+1; 
+      b=b+1; 
+      } 
+   } 
     return 0;
 }
