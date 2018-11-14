a1414b2d16a6e5d90612fa3aa570d09c4477c407
ycuige1 <yuqing.cui@u-psud.fr>
2018-11-14 23:12:36 +0100

    fini exo45

--- src/main.c
+++ src/main.c
@@ -2,8 +2,33 @@
 //
 
 #include "my_header.h"
-
+#include "TP_lib.h"
 int main(void)
 {
+ int n,s=0;
+ n = LancerDe();
+ while(n!=1)
+  {
+      if(n%2==0)
+      {
+          s=s+n;
+      }
+      else if(n==3)
+      {
+          s=s*2;
+      }
+      else if(n==5)
+      {
+          if(s>=2)
+          {
+              s=s-2;
+          }
+          else
+          {
+              s=0;
+          }
+      }
+  }
+
     return 0;
 }
