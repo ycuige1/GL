17b7759240967eb13713595626335fe7bcad3694
ycuige1 <yuqing.cui@u-psud.fr>
2018-11-14 20:00:50 +0100

    fini exo44

--- src/main.c
+++ src/main.c
@@ -2,8 +2,14 @@
 //
 
 #include "my_header.h"
-
+#include "TP_lib.h"
 int main(void)
 {
+    short resultat, i=0;
+    while(i<10)
+    {
+        resultat=LancerDe ();
+        i++;
+    }
     return 0;
 }
