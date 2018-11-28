6983b288a043a57677800cb9bced693d63098e2f
ycuige1 <yuqing.cui@u-psud.fr>
2018-11-28 21:23:13 +0100

    finir exo58

--- src/main.c
+++ src/main.c
@@ -3,37 +3,32 @@
 
 #include "my_header.h"
 #include "TP_lib_suite.h"
+void  initialiserTirage(void);
+short tirerNumero(void);
+
 int main(void)
 {
-    int joueur[6]={34,12,9,17,33,2},gagnante[6]={17,33,9,35,3,7},valeur,i,n;
-    int joueur2[6]={tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero()},gagnante2[6],valeur2,m,o;
-    for(i=0;i<6;i++)
+    unsigned char joueur[6]={34,12,9,17,33,2},gagnante[6],valeur=0,k,i,n,q;
+    for(q=0;q<100;q++)
     {
+        for(k=0;k<6;k++)
+     {
+        initialiserTirage();
+        gagnante[k]=tirerNumero();
+     }
+
+        for(i=0;i<6;i++)
+     {
         for(n=0;n<6;n++)
         {
-            if(joueur[n]==gagnante[i])
+            if(joueur[i]==gagnante[n])
             {
                 valeur=valeur+1;
             }
 
         }
+     }
+
     }
-    
-     initialiserTirage(void)
-
-        for(m=0;m<6;m++)
-    {
-        for(o=0;o<6;o++)
-        {
-            if(joueur2[m]==gagnante2[o])
-            {
-                valeur2=valeur2+1;
-            }
-
-        }
-    }
-
-
-
     return 0;
 }
