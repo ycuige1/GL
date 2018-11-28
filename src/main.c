8e60309fdd4f415173f4657b7a4553e8c5e493be
ycuige1 <yuqing.cui@u-psud.fr>
2018-11-28 12:22:22 +0100

    pas encore fini

--- src/main.c
+++ src/main.c
@@ -2,8 +2,38 @@
 //
 
 #include "my_header.h"
-
+#include "TP_lib_suite.h"
 int main(void)
 {
+    int joueur[6]={34,12,9,17,33,2},gagnante[6]={17,33,9,35,3,7},valeur,i,n;
+    int joueur2[6]={tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero(),tirerNumero()},gagnante2[6],valeur2,m,o;
+    for(i=0;i<6;i++)
+    {
+        for(n=0;n<6;n++)
+        {
+            if(joueur[n]==gagnante[i])
+            {
+                valeur=valeur+1;
+            }
+
+        }
+    }
+    
+     initialiserTirage(void)
+
+        for(m=0;m<6;m++)
+    {
+        for(o=0;o<6;o++)
+        {
+            if(joueur2[m]==gagnante2[o])
+            {
+                valeur2=valeur2+1;
+            }
+
+        }
+    }
+
+
+
     return 0;
 }
