25ee28377f4066b416ee3755f6983866910ea23d
yuqing cui <yuqing.cui@ge1.iut-cachan.u-psud.fr>
2018-12-04 11:49:43 +0100

    en train de faire

--- src/main.c
+++ src/main.c
@@ -1,9 +1,25 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
+#include <ansi_c.h>
 #include "my_header.h"
 
 int main(void)
 {
+	char nom[100],prenom[100];
+	unsigned long int annee;
+	printf("veuiller taper votre nom,SVP\n");
+	scanf("%s",nom);
+	
+	printf("veuiller taper votre prenom,SVP\n");  
+	scanf("%s",prenom); 
+	
+	printf("veuiller taper votre annee de naissance,SVP\n");  
+	scanf("%lu",&annee);
+	printf("Je m'appelle %s %s,et j'ai 19ans.\n",nom,prenom);
+	
+	printf("merci\n");   
+	
+	
     return 0;
 }
